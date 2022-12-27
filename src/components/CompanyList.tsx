e3f05f8519c1d8602c3513f5f363e87ca3090185
suhdonghwi <hwidongsuh@gmail.com>
2022-12-28 00:58:54 +0900

    add separation between round 0 and round > 0

--- src/components/CompanyList.tsx
+++ src/components/CompanyList.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { useState } from "react";
-import { useCompanies, useRoundData } from "../firebase";
+import { useCompanies, useCurrentRound, useRoundData } from "../firebase";
 
 import { colors } from "../styles";
 import CompanyModal from "./CompanyModal";
@@ -25,17 +25,22 @@ const Item = styled.li({
   },
 });
 
-const Container = styled.div({
-  display: "grid",
-  gridTemplateRows: "1fr 1fr",
-  gridTemplateColumns: "2fr 1fr",
-  rowGap: "0.2rem",
+const Container = styled.div<{ detailed: boolean }>(
+  {
+    display: "grid",
+    gridTemplateRows: "1fr 1fr",
+    gridTemplateColumns: "2fr 1fr",
 
-  width: "100%",
-  fontSize: "1.2rem",
+    width: "100%",
+    fontSize: "1.2rem",
 
-  padding: "0 0 0 0.7rem",
-});
+    paddingLeft: "0.7rem",
+  },
+  ({ detailed }) => ({
+    gridTemplateColumns: detailed ? "2fr 1fr" : "1fr",
+    rowGap: detailed ? "0.2rem" : 0,
+  })
+);
 
 const CompanyTitle = styled.div({
   fontWeight: 600,
@@ -71,27 +76,20 @@ const Change = styled.div<{ minus: boolean }>(
   })
 );
 
-type CompanyData = {
-  company: Company;
-  valuation: number;
-  investAmount: number;
-  change: number | null;
-};
-
 type CompanyListProps = {
   className?: string;
-  round: number;
   teamID: number;
 };
 
-function CompanyList({ className, round, teamID }: CompanyListProps) {
+function CompanyList({ className, teamID }: CompanyListProps) {
   const companies = useCompanies();
+  const round = useCurrentRound();
   const roundData = useRoundData();
 
   const [selectedCompany, setSelectedCompany] = useState<Company | null>(null);
   const [showModal, setShowModal] = useState(false);
 
-  function handleClickItem({ company }: CompanyData) {
+  function handleClickItem(company: Company) {
     setSelectedCompany(company);
     setShowModal(true);
   }
@@ -100,51 +98,60 @@ function CompanyList({ className, round, teamID }: CompanyListProps) {
     setShowModal(false);
   }
 
-  if (companies === null || roundData === null) return null;
+  if (companies === null || round === null || roundData === null) return null;
 
   const companiesList = Object.entries(companies).sort(([, a], [, b]) =>
     a.name.localeCompare(b.name)
   );
-  const companiesData: CompanyData[] = companiesList.map(
-    ([companyID, company]) => {
-      const valuation = roundData[round].valuation[companyID];
+  const showDetail = round > 0;
 
-      let change = null;
-      if (round !== 1) {
-        const prevValuation = roundData[round - 1].valuation[companyID];
-        change = (valuation / prevValuation - 1) * 100;
-      }
+  const DetailedContainer = ({
+    companyID,
+    company,
+  }: {
+    companyID: string;
+    company: Company;
+  }) => {
+    const valuation = roundData[round].valuation[companyID];
+    const change =
+      round === 1
+        ? null
+        : (valuation / roundData[round - 1].valuation[companyID] - 1) * 100;
+    const investAmount = roundData[round].investAmount[teamID][companyID];
 
-      return {
-        company,
-        valuation,
-        investAmount: roundData[round].investAmount[teamID][companyID],
-        change,
-      };
-    }
-  );
+    return (
+      <Container detailed>
+        <CompanyTitle>{company.name}</CompanyTitle>
+        <Valuation>{valuation.toLocaleString("en")}</Valuation>
+        <CompanySubtitle>
+          {investAmount
+            ? `투자액 ${investAmount.toLocaleString("en")}원`
+            : "투자하지 않음"}
+        </CompanySubtitle>
+        {change !== null && (
+          <Change minus={change < 0}>
+            {change >= 0 ? "+" : ""}
+            {change.toPrecision(3)}%
+          </Change>
+        )}
+      </Container>
+    );
+  };
 
   return (
     <>
       <List className={className}>
-        {companiesData.map((data) => (
-          <Item key={data.company.name} onClick={() => handleClickItem(data)}>
-            <CompanyLogo src={data.company.logo} width={56} />
-            <Container>
-              <CompanyTitle>{data.company.name}</CompanyTitle>
-              <Valuation>{data.valuation.toLocaleString("en")}</Valuation>
-              <CompanySubtitle>
-                {data.investAmount
-                  ? `투자액 ${data.investAmount.toLocaleString("en")}원`
-                  : "투자하지 않음"}
-              </CompanySubtitle>
-              {data.change !== null && (
-                <Change minus={data.change < 0}>
-                  {data.change >= 0 ? "+" : ""}
-                  {data.change.toPrecision(3)}%
-                </Change>
-              )}
-            </Container>
+        {companiesList.map(([companyID, company]) => (
+          <Item key={company.name} onClick={() => handleClickItem(company)}>
+            <CompanyLogo src={company.logo} width={56} />
+            {showDetail ? (
+              <DetailedContainer companyID={companyID} company={company} />
+            ) : (
+              <Container detailed={false}>
+                <CompanyTitle>{company.name}</CompanyTitle>
+                <CompanySubtitle>{company.engName}</CompanySubtitle>
+              </Container>
+            )}
           </Item>
         ))}
       </List>
