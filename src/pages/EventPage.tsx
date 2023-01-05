3ff95ba7596e8c831e2d6ca68001e03070eaabf8
suhdonghwi <hwidongsuh@gmail.com>
2023-01-05 15:45:11 +0900

    add event logo

--- src/pages/EventPage.tsx
+++ src/pages/EventPage.tsx
@@ -1,45 +1,48 @@
 import styled from "@emotion/styled";
+import { colors } from "@/styles";
 
 import Button from "@/components/Button";
+import Header from "@/components/Header";
 
 import BingoLogo from "@/assets/events/bingo-logo.png";
 import CompletionLogo from "@/assets/events/completion-logo.png";
-import { colors } from "@/styles";
 
 const Main = styled.main({
   display: "flex",
   flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
 
   height: "100%",
   width: "100%",
 
   margin: "0 auto",
-  padding: "1.5rem 0",
+  padding: "1.5rem 1rem",
 
   overflowY: "scroll",
+});
+
+const Title = styled(Header)({
+  marginBottom: "1.5rem",
 });
 
 const EventButton = styled(Button)({
   backgroundColor: colors.lightGray,
   border: `2px solid ${colors.gray}`,
-  padding: "1rem 2rem"
+  marginBottom: "1rem",
 });
 
-const LogoImage = styled.img({
+const EventLogo = styled.img({
   width: 200,
 });
 
 export default function EventPage() {
   return (
     <Main>
+      <Title as="h1">이벤트</Title>
       <EventButton>
-        <LogoImage src={BingoLogo} />
+        <EventLogo src={BingoLogo} />
       </EventButton>
-
       <EventButton>
-        <LogoImage src={BingoLogo} />
+        <EventLogo src={CompletionLogo} />
       </EventButton>
     </Main>
   );
